5ec1cc53ba830afbca46bf766cf2c11acb8db25c
0xbyte <N/A>
2020-07-29 18:06:09 +0100

    Add Lambda function for getting status and IP address of Minecraft server

---
+++ modules/powerswitch/bin/get_server_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import boto3
+import os
+
+instances = [os.getenv('MINECRAFT_SERVER_INSTANCE_ID')]
+region = os.getenv('MINECRAFT_SERVER_REGION')
+
+def lambda_handler(event, context):
+  ec2 = boto3.client('ec2', region_name = region)
+  response = ec2.describe_instances(InstanceIds=instances)
+  ip = response['Reservations'][0]['Instances'][0].get('PublicIpAddress')
+  status = response['Reservations'][0]['Instances'][0]['State']['Name']
+  return {
+    'statusCode': 200,
+    'body': f'Server is currently {status} with an IP address of {ip}.'
+  }
--- modules/powerswitch/main.tf
+++ modules/powerswitch/main.tf
@@ -60,6 +60,11 @@ resource "aws_lb_target_group" "stop_server_tg" {
   target_type = "lambda"
 }
 
+resource "aws_lb_target_group" "get_server_status_tg" {
+  name = "${module.powerswitch_label.id}-getstatus"
+  target_type = "lambda"
+}
+
 resource "aws_lb_listener_rule" "start_server_endpoint_rule" {
   listener_arn = aws_lb_listener.listener.arn
   action {
@@ -70,20 +75,6 @@ resource "aws_lb_listener_rule" "start_server_endpoint_rule" {
     path_pattern {
       values = [
         "/start"]
-    }
-  }
-}
-
-resource "aws_lb_listener_rule" "stop_server_endpoint_rule" {
-  listener_arn = aws_lb_listener.listener.arn
-  action {
-    type = "forward"
-    target_group_arn = aws_lb_target_group.stop_server_tg.arn
-  }
-  condition {
-    path_pattern {
-      values = [
-        "/stop"]
     }
   }
 }
@@ -115,6 +106,33 @@ module "start_server_function" {
 EOF
 }
 
+resource "aws_lambda_permission" "start_server_permission" {
+  action = "lambda:InvokeFunction"
+  function_name = module.start_server_function.this_lambda_function_arn
+  principal = "elasticloadbalancing.amazonaws.com"
+  source_arn = aws_lb_target_group.start_server_tg.arn
+}
+
+resource "aws_lb_target_group_attachment" "attach_start_server_function" {
+  target_group_arn = aws_lb_target_group.start_server_tg.arn
+  target_id        = module.start_server_function.this_lambda_function_arn
+  depends_on       = [aws_lambda_permission.start_server_permission]
+}
+
+resource "aws_lb_listener_rule" "stop_server_endpoint_rule" {
+  listener_arn = aws_lb_listener.listener.arn
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.stop_server_tg.arn
+  }
+  condition {
+    path_pattern {
+      values = [
+        "/stop"]
+    }
+  }
+}
+
 module "stop_server_function" {
   source = "terraform-aws-modules/lambda/aws"
   function_name = "${module.powerswitch_label.id}-stop-server"
@@ -142,19 +160,6 @@ module "stop_server_function" {
 EOF
 }
 
-resource "aws_lambda_permission" "start_server_permission" {
-  action = "lambda:InvokeFunction"
-  function_name = module.start_server_function.this_lambda_function_arn
-  principal = "elasticloadbalancing.amazonaws.com"
-  source_arn = aws_lb_target_group.start_server_tg.arn
-}
-
-resource "aws_lb_target_group_attachment" "attach_start_server_function" {
-  target_group_arn = aws_lb_target_group.start_server_tg.arn
-  target_id        = module.start_server_function.this_lambda_function_arn
-  depends_on       = [aws_lambda_permission.start_server_permission]
-}
-
 resource "aws_lambda_permission" "stop_server_permission" {
   action = "lambda:InvokeFunction"
   function_name = module.stop_server_function.this_lambda_function_arn
@@ -166,4 +171,58 @@ resource "aws_lb_target_group_attachment" "attach_stop_server_function" {
   target_group_arn = aws_lb_target_group.stop_server_tg.arn
   target_id        = module.stop_server_function.this_lambda_function_arn
   depends_on       = [aws_lambda_permission.stop_server_permission]
+}
+
+resource "aws_lb_listener_rule" "get_server_status_endpoint_rule" {
+  listener_arn = aws_lb_listener.listener.arn
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.get_server_status_tg.arn
+  }
+  condition {
+    path_pattern {
+      values = [
+        "/status"]
+    }
+  }
+}
+
+module "get_server_status_function" {
+  source = "terraform-aws-modules/lambda/aws"
+  function_name = "${module.powerswitch_label.id}-get-server-status"
+  description = "Function for getting the status of the Minecraft server instance"
+  handler = "get_server_status.lambda_handler"
+  runtime = "python3.8"
+  source_path = "${path.module}/bin/get_server_status.py"
+  environment_variables = {
+    MINECRAFT_SERVER_INSTANCE_ID = var.server_instance_id
+    MINECRAFT_SERVER_REGION = var.region
+  }
+  tags = module.powerswitch_label.tags
+  attach_policy_json = true
+  policy_json = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "ec2:DescribeInstances",
+      "Resource": "*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_permission" "get_server_status_permission" {
+  action = "lambda:InvokeFunction"
+  function_name = module.get_server_status_function.this_lambda_function_arn
+  principal = "elasticloadbalancing.amazonaws.com"
+  source_arn = aws_lb_target_group.get_server_status_tg.arn
+}
+
+resource "aws_lb_target_group_attachment" "attach_get_server_status_function" {
+  target_group_arn = aws_lb_target_group.get_server_status_tg.arn
+  target_id        = module.get_server_status_function.this_lambda_function_arn
+  depends_on       = [aws_lambda_permission.get_server_status_permission]
 }
